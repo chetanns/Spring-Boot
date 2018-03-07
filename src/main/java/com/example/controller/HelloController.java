85ffffcbf20a20c55d4b78253e8f3184db3994bc
chetanns <chetanns@gmail.com>
2018-03-06 22:08:12 -0500

    new changes

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -3,12 +3,29 @@ package com.example.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.annotation.ApiDoc;
+
 @RestController
+@RequestMapping("/Test")
 public class HelloController {
 	
 	@RequestMapping("/")
+	@ApiDoc("This method prints the display name")
 	public String index() {
 		return "Greeting from Spring Boot";
+	}
+	
+	
+	@RequestMapping("/Test1")
+	@ApiDoc("This method prints the message 1")
+	public String getMessage1() {
+		return "Message 1 from Spring Boot";
+	}
+	
+	@RequestMapping("/Test2")
+	@ApiDoc("This method prints the message 2")
+	public String getMessage2() {
+		return "Message 2 from Spring Boot";
 	}
 
 }
